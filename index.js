69cad5a2da578dbf52650252b94c03771593a4c4
diego <diegocarnicero2007@gmail.com>
2026-04-20 11:09:45 +0200

    añado el efecto de poner de un color el texto segun lo que pase

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const seleccionInput = document.querySelector("input");
 
 const seleccionButton = document.querySelector("button");
 
+const texto1 = document.getElementById("neto");
+const texto2 = document.getElementById("recibido");
+
 
 const neto = document.getElementById("montoNeto");
 const recibido = document.getElementById("porcentaje");
@@ -14,8 +17,18 @@ seleccionButton.addEventListener("click", () => {
 
   if (salario === 30000) {
     retencion = 20;
+
+    texto1.scrollIntoView({ behavior: "smooth" });
+    texto1.classList.add("coloreo");
+     texto2.scrollIntoView({ behavior: "smooth" });
+    texto2.classList.add("coloreo");
   } else {
     retencion = 15;
+
+    texto1.scrollIntoView({ behavior: "smooth" });
+    texto1.classList.add("coloreado");
+     texto2.scrollIntoView({ behavior: "smooth" });
+    texto2.classList.add("coloreado");
   }
   recibido.textContent = retencion;
 
